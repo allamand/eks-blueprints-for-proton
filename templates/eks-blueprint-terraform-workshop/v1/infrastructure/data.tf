fb99b56b09559ce49badb759e85f28ed205c66b7
sallaman <sallaman@amazon.com>
2022-07-11 21:57:28 +0000

    fix output

--- templates/eks-blueprint-terraform-workshop/v1/infrastructure/data.tf
+++ templates/eks-blueprint-terraform-workshop/v1/infrastructure/data.tf
@@ -4,6 +4,10 @@ data "aws_partition" "current" {}
 # Find the user currently in use by AWS
 data "aws_caller_identity" "current" {}
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks_blueprints.eks_cluster_id
+}
+
 # Region in which to deploy the solution
 data "aws_region" "current" {}
 
--- templates/eks-blueprint-terraform-workshop/v1/infrastructure/outputs.tf
+++ templates/eks-blueprint-terraform-workshop/v1/infrastructure/outputs.tf
@@ -1,9 +1,17 @@
 output "platform_teams_configure_kubectl" {
   description = "The command to use to configure the kubeconfig file to be used with kubectl."
   value = tomap({
-    for k, v in module.eks_blueprints.teams[0].platform_teams_iam_role_arn : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${data.aws_eks_cluster.cluster.name}  --role-arn ${v}"
-  })["platform-team"]
+    #for k, v in module.eks_blueprints.teams[0].platform_teams_iam_role_arn : 
+    #k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig 
+    #--name ${data.aws_eks_cluster.cluster.name}  --role-arn ${v}"
+    for k, v in module.eks_blueprints.teams[0].platform_teams_iam_role_arn : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${module.eks_blueprints.eks_cluster_id}  --role-arn ${v}"
+  })["admin"]
 }
+output "platform_team" {
+  description = "Role Arn of platform-team"
+  value       = module.eks_blueprints.teams[*].platform_teams_iam_role_arn["admin"]
+}
+
 
 output "eks_cluster_id" {
   description = "The name of the EKS cluster."
