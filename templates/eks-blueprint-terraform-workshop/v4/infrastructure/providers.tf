4a764c852e28984bcc7b9467376d5c64582ff8ff
sallaman <sallaman@amazon.com>
2022-09-02 07:23:11 +0000

    add v4

---
+++ templates/eks-blueprint-terraform-workshop/v4/infrastructure/providers.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = var.aws_region
+  alias  = "default"
+  default_tags {
+    tags = {
+      "proton:environment" = var.environment.name
+    }
+  }
+}
+
+variable "aws_region" {
+  type = string
+}
+
+terraform {
+  required_version = ">= 1.0.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+  }
+}
