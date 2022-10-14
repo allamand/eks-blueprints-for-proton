45064b256fc250aa72b510060236f3ad395459cf
sbx_user1051 <sbx_user1051@169.254.88.1>
2022-10-14 13:29:28 +0000

    Update requested for Proton - environment/proton-blue

--- proton-blue/main.tf
+++ proton-blue/main.tf
@@ -1,9 +1,5 @@
 /*
-This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
-
-To manage this resource, see AWS Proton Resource: arn:aws:proton:eu-west-1:382076407153:environment/proton-blue
-
-If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
 */
 
 provider "kubernetes" {
