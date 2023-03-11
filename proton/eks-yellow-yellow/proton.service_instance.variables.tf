409f5009c1e02648f4d917067ce7e43649e7727a
sbx_user1051 <sbx_user1051@169.254.180.169>
2023-03-11 16:59:15 +0000

    Update requested for Proton - service/eks-yellow/service-instance/yellow

---
+++ proton/eks-yellow-yellow/proton.service_instance.variables.tf
@@ -0,0 +1,39 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:eu-west-1:382076407153:service/eks-yellow/service-instance/yellow
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+variable "environment" {
+  type = object({
+    account_id = string
+    name       = string
+    outputs    = map(string)
+  })
+  default = null
+}
+
+variable "service" {
+  type = object({
+    name                      = string
+    branch_name               = string
+    repository_connection_arn = string
+    repository_id             = string
+  })
+}
+
+variable "service_instance" {
+  type = object({
+    name       = string
+    inputs     = any
+    components = map(any)
+  })
+  default = null
+}
+
+variable "proton_tags" {
+  type    = map(string)
+  default = null
+}
