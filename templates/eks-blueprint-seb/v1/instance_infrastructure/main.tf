cc79f348363167a12e45316ec860c1a60db561e2
sallaman <sallaman@amazon.com>
2023-03-11 20:25:47 +0000

    update template 4

--- templates/eks-blueprint-seb/v1/instance_infrastructure/main.tf
+++ templates/eks-blueprint-seb/v1/instance_infrastructure/main.tf
@@ -249,6 +249,10 @@ data "aws_partition" "current" {}
 # Find the user currently in use by AWS
 data "aws_caller_identity" "current" {}
 
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks_cluster.eks_cluster_id
+}
+
 data "aws_vpc" "vpc" {
   filter {
     name   = "tag:Name"
