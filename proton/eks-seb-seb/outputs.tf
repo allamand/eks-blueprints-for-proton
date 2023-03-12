b80ce2673ef2b26e34ee2c26a7d1391f6e0c5936
sbx_user1051 <sbx_user1051@169.254.12.105>
2023-03-12 13:45:54 +0000

    Update requested for Proton - service/eks-seb/service-instance/seb

---
+++ proton/eks-seb-seb/outputs.tf
@@ -0,0 +1,66 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:eu-west-1:382076407153:service/eks-seb/service-instance/seb
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+
+
+# output "platform_teams_configure_kubectl" {
+#   description = "The command to use to configure the kubeconfig file to be used with kubectl."
+#   value = tomap({
+#     #for k, v in module.eks_blueprints.teams[0].platform_teams_iam_role_arn : 
+#     #k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig 
+#     #--name ${data.aws_eks_cluster.cluster.name}  --role-arn ${v}"
+#     for k, v in module.eks_blueprints.teams[0].platform_teams_iam_role_arn : k => "aws eks --region ${data.aws_region.current.id} update-kubeconfig --name ${module.eks_blueprints.eks_cluster_id}  --role-arn ${v}"
+#   })["admin"]
+# }
+
+output "eks_cluster_id" {
+  description = "The name of the EKS cluster."
+  value       = module.eks_blueprints.eks_cluster_id
+}
+
+output "kubernetes_version" {
+  description = "The version of the EKS cluster."
+  value       = var.service_instance.inputs.kubernetes_version
+}
+
+output "enable_aws_load_balancer_controller" {
+  description = "The flag for the Load Balancer controller."
+  value       = var.service_instance.inputs.aws_load_balancer_controller
+}
+
+output "enable_karpenter" {
+  description = "The flag for Karpenter."
+  value       = var.service_instance.inputs.karpenter
+}
+
+output "enable_metrics_server" {
+  description = "The flag for the Metric Server."
+  value       = var.service_instance.inputs.metrics_server
+}
+
+output "enable_aws_for_fluentbit" {
+  description = "The flag for the Fluentbit."
+  value       = var.service_instance.inputs.aws_for_fluentbit
+}
+
+output "enable_cert_manager" {
+  description = "The flag for Certificate Manager."
+  value       = var.service_instance.inputs.cert_manager
+}
+
+output "enable_vpa" {
+  description = "The flag for Virtual Pod Autoscaler."
+  value       = var.service_instance.inputs.vpa
+}
+
+
+
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = module.eks_blueprints.configure_kubectl
+}
