0742bea075aceeeb3449ee2b60a75519c42ee8a1
sallaman <sallaman@amazon.com>
2023-03-12 13:11:20 +0000

    update template 5

--- templates/eks-blueprint-seb/v1/instance_infrastructure/main.tf
+++ templates/eks-blueprint-seb/v1/instance_infrastructure/main.tf
@@ -250,7 +250,7 @@ data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks_cluster.eks_cluster_id
+  name = module.eks_blueprints.eks_cluster_id
 }
 
 data "aws_vpc" "vpc" {
