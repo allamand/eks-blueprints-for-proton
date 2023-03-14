7ba0b1879dfeecf7f3a49f9e1b26a81e77928f1c
sallaman <sallaman@amazon.com>
2023-03-14 17:28:00 +0000

    add variable proton-terraform mapping

--- templates/eks-core-infra-codebuild/v1/infrastructure/variables.tf
+++ templates/eks-core-infra-codebuild/v1/infrastructure/variables.tf
@@ -3,3 +3,13 @@ variable "aws_region" {
   description = "AWS Region"
   type        = string
 }
+
+# required by proton
+variable "environment" {
+  description = "The Proton Environment"
+  type = object({
+    name   = string
+    inputs = map(string)
+  })
+  default = null
+}
