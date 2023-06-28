15411e7517b37cb47c1549e936791aa68fcd921f
sallaman <sallaman@amazon.com>
2023-06-28 13:35:26 +0000

    simplify output for proton3

--- templates/eks-blueprint-codebuild/v2/instance_infrastructure/outputs.tf
+++ templates/eks-blueprint-codebuild/v2/instance_infrastructure/outputs.tf
@@ -43,6 +43,8 @@ output "eks_blueprints_platform_teams_configure_kubectl" {
   value       = module.eks_cluster.eks_blueprints_platform_teams_configure_kubectl
 }
 
+#Proton does not like list of outputs
+#Invalid type for parameter outputs[0].valueString, value: ['aws eks --region eu-west-1 update-kubeconfig --name proton-dev-eks-green  --role-arn arn:aws:iam::382076407153:role/team-burnham-20230627141152607800000015', 'aws eks --region eu-west-1 update-kubeconfig --name proton-dev-eks-green  --role-arn arn:aws:iam::382076407153:role/team-riker-20230627141152922700000016'], type: <class 'list'>, valid types: <class 'str'>
 /* output "eks_blueprints_dev_teams_configure_kubectl" {
   description = "Configure kubectl for each Dev Application Teams: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value       = module.eks_cluster.eks_blueprints_dev_teams_configure_kubectl
