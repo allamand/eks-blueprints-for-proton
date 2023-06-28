35a128a2f467d34b1deda10cf24ae39773a39fb5
sallaman <sallaman@amazon.com>
2023-06-28 12:47:36 +0000

    fix login

--- scripts/bash/functions.sh
+++ scripts/bash/functions.sh
@@ -40,5 +40,7 @@ configure_terraform_init()
   export PROTON_SVC_INSTANCE=$(cat proton-inputs.json | jq '.service_instance.name' -r)
   export KEY=${PROTON_ENV}.${PROTON_SVC}.${PROTON_SVC_INSTANCE}
   [[ -z $PROTON_ENV || -z $PROTON_SVC || -z $PROTON_SVC_INSTANCE ]] && echo "Error: One or more variables are missing or empty" && return 1
+  set -x
   terraform init -backend-config="bucket=${TF_STATE_BUCKET}" -backend-config="region=${TF_STATE_BUCKET_REGION}" -backend-config="key=${KEY}.tfstate"
+  set +x
 }
